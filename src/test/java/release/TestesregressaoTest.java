f65e98409e64003e13c323527f71f27b06e2f96e
jsobr <jsobr@DESKTOP-F65L6T1>
2018-06-25 16:00:18 -0300

    Teste Commit 02

--- src/test/java/release/TestesregressaoTest.java
+++ src/test/java/release/TestesregressaoTest.java
@@ -10,17 +10,17 @@ import cucumber.api.junit.Cucumber;
 	glue = { "" }, monochrome = true, dryRun = false,
 
     tags  = {
-//            "@CenarioCombo3P-B2C,"
-//           + "@CenarioCombo2P-B2C,"
-//           + "@CenarioBL-B2C,"
-//           + "@CenarioFixa-B2C,"
-//           + "@CenarioTV-B2C,"
-//           + "@CenarioSimu3P-B2C,"
-//           + "@CenarioFixa02-B2C,"
-//           + "@CenarioSimu2P-B2C,"
-//           + "@CenarioSimu2P-B2B,"
-//           + "@CenarioCombo3P-B2B"
-			  "@CenarioSimu2PSP-B2B,"
+            "@CenarioCombo3P-B2C,"
+           + "@CenarioCombo2P-B2C,"
+           + "@CenarioBL-B2C,"
+           + "@CenarioFixa-B2C,"
+           + "@CenarioTV-B2C,"
+           + "@CenarioSimu3P-B2C,"
+           + "@CenarioFixa02-B2C,"
+           + "@CenarioSimu2P-B2C,"
+           + "@CenarioSimu2P-B2B,"
+           + "@CenarioCombo3P-B2B"
+			+  "@CenarioSimu2PSP-B2B,"
 			+ "@CenarioCombo3PSP-B2B"
         	})
 
